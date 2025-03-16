d81463a1f2cb5eea41fdd420e0801606040735b4
vishal-patidar-02 <vishalpatidar07070707@gmail.com>
2025-03-16 16:29:39 +0530

    fix 1

--- server/server.js
+++ server/server.js
@@ -1,21 +1,24 @@
-import  'dotenv/config';
-import express from 'express';
-import cors from 'cors';
-import connectDB from './configs/mongodb.js';
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import connectDB from "./configs/mongodb.js";
 
-// App config 
+// App config
 const app = express();
 const port = process.env.PORT || 4000;
 await connectDB();
 
-// Middleware 
+// Middleware
 app.use(express.json());
 app.use(cors());
 
-// API ROute 
-app.get('/', (req, res) => res.send("App is working"));
+// API ROute
+app.get("/", (req, res) => {
+  console.log("App is working");
+
+  res.send("App is working");
+});
 
 app.listen(port, () => {
-    console.log("Server is running on port: ", port);
-    
-})
+  console.log("Server is running on port: ", port);
+});
